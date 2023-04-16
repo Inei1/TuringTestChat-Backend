b6b7a340f6bb8fd4e5c27e7708ae06300300070a
Inei1 <neilrmoon@gmail.com>
2023-04-15 18:06:08 -0700

    Make the chat automatically communicate with chatgpt

--- src/server/ChatServer.ts
+++ src/server/ChatServer.ts
@@ -6,15 +6,38 @@ import ChatController from '../controllers/ChatController';
 var https = require('https');
 var http = require('http');
 import { readFileSync } from 'fs';
-const cors = require('cors');
+import cors = require('cors');
+import { Configuration, OpenAIApi } from 'openai';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 class ChatServer extends Server {
 
   chatRoom = '';
   allUsers: any[] = [];
+  apiKey = process.env.OPENAI_API_KEY;
+  config = new Configuration({
+    apiKey: this.apiKey,
+  });
+  openai = new OpenAIApi(this.config);
+  messages: any[] = [{ role: "system", content: "You are trying to convince the person on the other side that you are a human." }];
+  isBot = true;
+  wordsPerSecond = 20;
+  // make between 30-80 wpm (0.5-1.33), weighted more to low numbers
 
   private readonly DEV_MSG = 'Express Server is running in development mode. ' +
     'No front-end content is being served.';
+
+  convertMessage = (data: any) => {
+    this.messages.push({ role: data.name, content: data.text });
+    // console.log(data.text);
+    // if (data.name === "user1") {
+    //   this.messages.push({ role: "user", content: data.text });
+    // } else {
+    //   this.messages.push({ role: "user1", content: data.text });
+    // }
+  }
 
   constructor() {
     super(true);
@@ -52,8 +75,22 @@ class ChatServer extends Server {
     io.on("connection", (socket) => {
       logger.info("User connected: " + socket.id);
 
-      socket.on("message", (data) => {
+      socket.on("message", async (data) => {
         io.emit("messageResponse", data);
+
+        this.convertMessage(data);
+        const completion = await this.openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: this.messages,
+        });
+        setTimeout(() => io.emit("typingResponse", "user2"), 100);
+        const message = completion.data.choices[0].message?.content;
+        
+        setTimeout(() => io.emit("messageResponse", {
+          name: "user2",
+          text: completion.data.choices[0].message?.content
+        }), (message?.length! / this.wordsPerSecond) * 1000);
+        io.emit("typingResponse", "");
       });
 
       socket.on("typing", (data) => socket.broadcast.emit("typingResponse", data));
