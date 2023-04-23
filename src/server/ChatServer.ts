aab562c1f3737c980e209b58511ca8e93c53443b
Inei1 <neilrmoon@gmail.com>
2023-04-22 21:56:52 -0700

    Remove cors middleware

--- src/server/ChatServer.ts
+++ src/server/ChatServer.ts
@@ -62,7 +62,6 @@ class ChatServer extends Server {
     connectToDatabase().then((collections) => globalThis.collections = collections);
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: true }));
-    this.app.use(cors());
 
     passport.serializeUser((user, done) => {
       done(undefined, user);
