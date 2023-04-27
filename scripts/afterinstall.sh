5048d7bc1e58b140fae9ee7b507169cfcc5b956b
Inei1 <neilrmoon@gmail.com>
2023-04-27 14:47:18 -0700

    update afterinstall

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -8,6 +8,7 @@ sudo npm i typescript
 sudo /home/ubuntu/temp/node_modules/typescript/bin/tsc
 cp /home/ubuntu/temp/package*.json /home/ubuntu/temp/build
 cp /home/ubuntu/*.pem /home/ubuntu/temp/build/
+cp /home/ubuntu/.env /home/ubuntu/temp/build/
 cd /home/ubuntu/temp/build/
 sudo npm i --omit=dev
 cd /home/ubuntu
